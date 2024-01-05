d379009a5f6c18f6fdb77b0e31aead28cdd61b44
oyollobrian <oyollobrian@gmail.com>
2024-01-05 22:28:21 +0300

    fixed db bug

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,6 @@ host_port = os.environ['HOST_PORT']
 
 
 connection_string = f"postgresql://{username}:{password}@{db_host}:{host_port}/{dbname}_test"
-
 engine = create_engine(connection_string)
 TestsessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
